ebaec8ef1467300caf87d5ab24cef0b107e3f169
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-22 06:32:15 +0100

    Adding More Buttons

--- Assets/Graphs/Graph.cs
+++ Assets/Graphs/Graph.cs
@@ -41,6 +41,12 @@ public class Graph
 
     public bool AStar(GameObject startId, GameObject endId)
     {
+        if(startId == endId)
+        {
+            pathList.Clear();
+            return false;
+        }
+
         Node start = FindNode(startId);
         Node end = FindNode(endId);
 
