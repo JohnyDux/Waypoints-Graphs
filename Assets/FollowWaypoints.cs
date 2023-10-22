4e355c8f3a1eb20fba7f447a5ce7c93f7781fe83
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-22 06:10:37 +0100

    Using A* with Waypoints Part 2

---
+++ Assets/FollowWaypoints.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoints : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0]; 
+    }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[0]);
+        currentWP = 0;
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[4]);
+        currentWP = 0;
+    }
+    
+    void Update()
+    {
+        
+    }
+}
